580147e452e0d7c28152a89a64ae0bc9b8cccc0c
Labaile <Labaile1@gmail.com>
2022-07-12 14:14:08 -0400

    Add new files "StringUtil" for unit testing intro: test for reserveNull (afterfix) both tests work

--- src/main/java/StringUtils.java
+++ src/main/java/StringUtils.java
@@ -1,6 +1,10 @@
 public class StringUtils {
     public String reverse(String toReverse) {
         // return "elpmas";
+        if (toReverse == null) {
+            return null;
+        }
+
         byte[] stringBytes = toReverse.getBytes();
         byte[] reversedBytes = new byte[stringBytes.length];
 
