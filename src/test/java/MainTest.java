21f773b8df08ac73ede7038a0414124c30947344
Labaile <Labaile1@gmail.com>
2022-07-12 14:21:29 -0400

    Added method signatures for test cases with Fizzbuzz

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,10 +1,22 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MainTest {
 
     @Test
     void main() {
+
+    }
+
+    @Test
+    void startwithf() {
+    }
+
+    @Test
+    void endwithb() {
+    }
+
+    @Test
+    void startswithfandendswithb() {
+
     }
 }
